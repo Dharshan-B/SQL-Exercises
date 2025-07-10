426fad522641e4cb2dee79c1aa809942fe91f636
Dharshan B <dharshanb0611@gmail.com>
2025-07-10 14:34:16 +0530

    Exercise3

--- Database_Design_and_Normalization/EX-2.sql
+++ Database_Design_and_Normalization/EX-2.sql
@@ -1,3 +1,24 @@
 --2. Resolving Partial Dependencies for Second Normal Form (2NF)
 
 --Write a SQL query to resolve partial dependencies and achieve 2NF.
+--ORIGINAL TABLE
+CREATE TABLE Orders(
+  OrdersID INT,
+  CustomerID INT,
+  CustomerName VARCHAR(100),
+  ProductID INT,
+  Quantity INT
+);
+--Resolving Partial Dependency by splitting into two tables 
+CREATE TABLE Customers(
+  CustomerID INT PRIMARY KEY,
+  CustomerName VARCHAR(100)
+);
+
+CREATE TABLE Orders_2NF(
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    ProductID INT,
+    Quantity INT,
+    FOREIGN KEY (CustomerID) References Customers(CustomerID)
+);
